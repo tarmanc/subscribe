a4dcdbe336e4824143721952112f71aa33260f29
adyingwish <62962574+adyingwish@users.noreply.github.com>
2020-05-04 20:28:30 +0300

    Initial Commit: Subscriber API

--- src/main/java/com/armanc/subscribe/controller/RestController.java
+++ src/main/java/com/armanc/subscribe/controller/RestController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
+
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("/subscribers")
 public class RestController {
 
     @Autowired
@@ -15,27 +17,26 @@ public class RestController {
 
 
     //Get a single user by ID
-    @GetMapping("/subscribers/{id}")
+    @GetMapping("/{id}")
     public Subscriber getSubById(@PathVariable long id) {
-        return service.getSubByID(id);
+            return service.getSubByID(id);
     }
 
     //Get a single user by user name
-    @GetMapping("/subscribers/user/{name}")
+    @GetMapping("/user/{name}")
     public Subscriber getSubByName(@PathVariable("name") String userName) {
         return service.getSubByName(userName);
     }
 
 
     //Update Subscription status by user name
-    @PatchMapping("/subscribers/user/{name}")
+    @PatchMapping("/user/{name}")
     public String subUnsubByName(@PathVariable("name") String name) {
-
         return service.subUnsub(name);
     }
 
     //Update Subscription status by id
-    @PatchMapping("/subscribers/{id}")
+    @PatchMapping("/{id}")
     public String subUnsubById(@PathVariable long id) {
         return service.subUnsub(id);
     }
